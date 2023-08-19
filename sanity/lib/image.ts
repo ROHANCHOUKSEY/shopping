09d3f3ed5c05103c54e311341b9a678b35cd5711
ROHANCHOUKSEY <91025012+ROHANCHOUKSEY@users.noreply.github.com>
2023-08-19 19:21:20 +0530

    Update image.ts

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,14 +1,13 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import type { Image } from 'sanity'
+import createImageUrlBuilder from '@sanity/image-url';
+import { SanityDocument } from '@sanity/client';
 
-import { dataset, projectId } from '../../sanity/env'
+import { dataset, projectId } from '../../sanity/env';
 
 const imageBuilder = createImageUrlBuilder({
   projectId: projectId || '',
   dataset: dataset || '',
-})
+});
 
-export const urlForImage = (source: Image) => {
-  return imageBuilder?.image(source).auto('format').fit('max')
-}
- 
+export const urlForImage = (source: SanityDocument) => {
+  return imageBuilder?.image(source).auto('format').fit('max');
+};
